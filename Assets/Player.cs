ceb1efab8accd1199a82b3d3f9deed2fd4e05717
unknown <songziyu@tjadri.com>
2023-05-26 17:24:14 +0800

    version 1.230526TJ

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move Info")]
+    [SerializeField] public float xSpeed;
+
+
+
+    #region Components
+    public Animator anim {  get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    #endregion
+    #region States
     public PlayerStateMachine stateMachine { get; private set; }
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
-
-
+    #endregion
+    
     private void Awake()
     {
         stateMachine = new PlayerStateMachine();
@@ -19,11 +29,18 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         stateMachine.Initialize(idleState);
     }
 
     private void Update()
     {
         stateMachine.currentState.Update();
+    }
+
+    public void SetVelocity(float _xInput, float _yInput)
+    {
+        rb.velocity = new Vector2(_xInput, _yInput);
     }
 }
--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerState
 {
+    protected float xInput;
+    public Rigidbody2D rb;
+
     protected PlayerStateMachine stateMachine;
     protected Player player;
     private string animBoolName;
@@ -16,17 +19,18 @@ public class PlayerState
 
     public virtual void Enter()
     {
-
+        player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
     }
 
     public virtual void Update()
     {
-
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 
     public virtual void Exit()
     {
-
+        player.anim.SetBool(animBoolName, false);
     }
 
 
